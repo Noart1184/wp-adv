bfd22618e88a6009c1e5a4b2e113175d2725b5bd
Noart1184 <noart.ramadani@students.askosova.org>
2025-11-08 16:41:41 +0100

    svsgs
    svsvsv

--- singel.php
+++ singel.php
@@ -3,12 +3,13 @@
 ?>
 <article class="content px-3 py-5 p-md-5">
     <?php
-    if(have_posts()){
-        while(have_posts()){
-            the_post();
-            get_template_part('template-part/content','atricle');
+        if(have_posts()) {
+            while(have_posts()){
+                the_post();
+                get_template_part('template-part/content', 'article');
+            }
         }
-    }
-    ?>
-    </article>
+        ?>
+</article>
+
 <?php get_footer(); ?>
--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,20 +1,33 @@
 <div class="container">
     <header class="content-header">
         <div class="meta mb-3"><span class="date"><?php echo get_the_date(); ?> </span>
-        <?php
-        the_tags('<span class="tag"><i class="fa fa-tag"></li', '</span><span class="tag"><i class="tag<>i class="fa fa-tag"></i>', '</span>');
+        <?php 
+        the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>');
         ?>
-        <span class="tag><i class="fa fa-tag></i>
-        <?php
-        $categories = get_the_category();
-        $sepearator = ' ';
-        $output = '';
-        if(!empty($categories)){
-            foreach($categories as $category) {
-                $output.='<a href_="'. esc_url(get_category_link($category->term_id)).'" alt="'.esc_attr(sprintf(esc_html_('View all post in %s', 'textdomain'),$category->name)).'">' 
-                esc_html($category->name) . '</a'. $seperator;
-            }
-        }
-        echo trim($output, $seperator);
-        ?>
-        </span>
+        <span class="tag"><i class="fa fa-tag"></i>
+            <?php
+                $categories = get_the_category();
+                $separator = ' ';
+                $output = '';
+                if(!empty($categories)){
+                    foreach($categories as $category) {
+                        $output.='<a href="'. esc_url(get_category_link($category->term_id)).'" alt="'.esc_attr(sprintf(esc_html__('View all posts in %s', 'textdomain'), $category->name)).'">' .
+                        esc_html($category->name) . '</a>'. $separator;
+                    }
+                }
+                echo trim($output, $separator);
+            ?>
+    </span>
+    <span class="comment"><a href="#comments"><i class="fa fa-comment"></i> <?php comments_number('0', '1', '%'); ?></a></span>
+        </div>
+    </header>
+</div>
+
+    <?php the_content(); ?>
+    <?php
+    comments_template(); 
+    ?>
+    
+        </div> 
+
+
